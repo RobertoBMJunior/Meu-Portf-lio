48a7edfc53d82a6dbaae7501d7d22defe2302a87
Roberto Junior <robertojuniorazul@gmail.com>
2023-03-14 01:34:30 -0300

    almost ready

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ btn_read_more.addEventListener('click',()=>{
         <span>
             Ver menos projetos
         </span>
-        `
+    `
     }
     
     btn_read_more.innerHTML = `   
@@ -24,3 +24,35 @@ btn_read_more.addEventListener('click',()=>{
     `
     
 })
+
+// --------------------Âncora de índice de conteudo---------------------
+// Aqui nós estaremos realizando o scroll da página para 100px abaixo
+// de onde ela está atualmente para o conteúdo não ficar abaixo do header
+function offsetAnchor() {
+    if (location.hash.length !== 0) {
+      window.scrollTo(window.scrollX, window.scrollY - 100  );
+    }
+  }
+  
+
+let list0 = document.querySelector('a[href^="#about-me"')
+let list1 = document.querySelector('a[href^="#ability"')
+let list2 = document.querySelector('a[href^="#my-projects"')
+
+let list = [list0,list1,list2]
+
+list.forEach(el => {
+    el.addEventListener("click", function() {
+    
+      window.setTimeout(function() {
+        // O clique é capturado antes da mudança do #, então
+        // o timeout faz com que esse código seja executado
+        // apenas após a rolagem do redirecionamento ser executada
+        offsetAnchor();
+      }, 0);
+  
+    });
+  })
+  
+  // Definimos o offset inicial caso a página aberta já esteja indo para um #elemento
+window.setTimeout(offsetAnchor, 0);
